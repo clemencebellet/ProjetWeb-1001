029d62a0973acada806e7c1fba43d2e98cfa0c8f
Rose <rose.martin@edu.ece.fr>
2022-05-25 00:00:35 +0200

    petite modif parce que ca beugait

--- rendezvous.php
+++ rendezvous.php
@@ -9,7 +9,7 @@ $db_mdp ="";
 $db_handle = mysqli_connect($site,$db_id,$db_mdp);
 $db_found = mysqli_select_db($db_handle,$db);
 
-$nom_coach =$_SESSION['NomCoach'];
+//$nom_coach =$_SESSION['NomCoach'];
 $nom =$_SESSION['Nom'];
 $id_client= $_SESSION['id'];
 
